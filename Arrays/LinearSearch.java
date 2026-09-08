3135f801b8ce55043d0d0a368145366e71a41442
shrey2a <shrey2a2006@gmail.com>
2026-09-08 08:42:17 +0530

    Add linear search program

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,24 @@
+// Linear Search
+
+public class LinearSearch {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 40, 15};
+
+        int target = 40;
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
