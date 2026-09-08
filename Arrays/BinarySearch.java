e1628409c1ed6cd361dd01e9a7d6e9aa621c7cc4
shrey2a <shrey2a2006@gmail.com>
2026-09-08 08:43:28 +0530

    Add binary search program

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,37 @@
+// Binary Search
+// Note: Binary Search works only on a sorted array.
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+
+        int target = 50;
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        boolean found = false;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Element found at index: " + mid);
+                found = true;
+                break;
+            } 
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
